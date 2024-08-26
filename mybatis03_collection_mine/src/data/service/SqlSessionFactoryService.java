a1623b96655a16c09a4193dc0136d5abd5726925
jaeeun <rosa2070@naver.com>
2024-08-26 20:33:00 +0900

    08/26

---
+++ mybatis03_collection_mine/src/data/service/SqlSessionFactoryService.java
@@ -0,0 +1,28 @@
+package data.service;
+
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class SqlSessionFactoryService {
+	private static SqlSessionFactory sqlSessionFactory;
+	
+	static {
+		try {
+			String resource="mybatis/mybatis-config.xml";
+			InputStream is = Resources.getResourceAsStream(resource);
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+		} catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static SqlSessionFactory getSqlSessionFactory() {
+		return sqlSessionFactory;
+	}
+	
+	
+
+}
---
+++ mybatis03_collection_mine/src/test/main/TestMain.java
@@ -0,0 +1,31 @@
+package test.main;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+
+import test.dao.MovieDao;
+
+public class TestMain {
+	public static void main(String[] args) {
+		MovieDao dao = MovieDao.getInstance();
+		List<HashMap<String, Object>> list = dao.commList1();
+		for(int i=0; i<list.size(); i++) {
+			HashMap<String, Object> map = list.get(i);
+			BigDecimal mnum = (BigDecimal) map.get("MNUM");
+			String title = (String) map.get("TITLE");
+			String director = (String) map.get("DIRECTOR");
+			BigDecimal num = (BigDecimal) map.get("NUM");
+			String id = (String) map.get("ID");
+			String comments = (String) map.get("COMMENTS");
+			System.out.println("영화번호:" + mnum);
+			System.out.println("제목:" + title);
+			System.out.println("감독:" + director);
+			System.out.println("댓글번호:" + num);
+			System.out.println("아이디:" + id);
+			System.out.println("댓글내용:" + comments);
+			System.out.println();
+		}
+	}
+
+}
